3557cdc0fad4c4fc0cbced2d76095ed2f20de2d3
Aashna Bara <aashnarose12@gmail.com>
2025-05-18 22:34:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,27 @@ import AboutMe from './pages/AboutMe'
 import Resume from './pages/Resume'
 import Contact from './pages/Contact'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-const App = () => {
-  //creating router : 
- const router = createBrowserRouter([{
-  path:"/", element:<MainNavigation/>, children:[
-    {path:"/", element:<Home/>},
-    {path:"/aboutMe", element:<AboutMe/>},
-    {path:"/resume", element:<Resume/>},
-    {path:"/contact", element:<Contact/>}
+
+  //creating router :
+ const router = createBrowserRouter(
+  [
+    {
+      path: "/", element: <MainNavigation />, children: [
+        { path: "/", element: <Home /> },
+        { path: "aboutMe", element: <AboutMe /> },
+        { path: "resume", element: <Resume /> },
+        { path: "contact", element: <Contact /> }
+      ]
+    }
   ],
   {
     basename: "/Portfolio"
   }
- }])
-  return (
-    <>
-    <RouterProvider router={router}></RouterProvider>
-    </>
-  )
-}
+);
+
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App
